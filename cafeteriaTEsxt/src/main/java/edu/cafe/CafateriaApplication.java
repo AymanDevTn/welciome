666ac02f9fbe2a62d486c7647e73f262a2130c1c
khouloud <khouloud@DESKTOP-MMBGMVM>
2024-07-29 13:56:21 +0100

    1

---
+++ cafeteriaTEsxt/src/main/java/edu/cafe/CafateriaApplication.java
@@ -0,0 +1,17 @@
+package edu.cafe;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CafateriaApplication  {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CafateriaApplication.class, args);
+	}
+
+
+
+}
